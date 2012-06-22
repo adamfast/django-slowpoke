2889b5695157192dc412f90ce096ba46eeabdfd7
Adam Fast <adam@adamfast.com>
2012-06-21 22:38:27 -0500

    Log what machine the test suite was run on, and track whether or not the standard for each test was met.

--- slowpoke/models.py
+++ slowpoke/models.py
@@ -10,6 +10,7 @@ TIME_STANDARDS = getattr(settings, 'TIME_STANDARDS', {
 class TestSuiteRun(models.Model):
     start = models.DateTimeField(null=True, blank=True)
     end = models.DateTimeField(null=True, blank=True)
+    machine = models.CharField(max_length=128)
 
 
 class TestRun(models.Model):
@@ -19,3 +20,24 @@ class TestRun(models.Model):
     args = models.TextField(null=True, blank=True)
     kwargs = models.TextField(null=True, blank=True)
     runtime_ms = models.PositiveIntegerField()
+    meets_standard = models.NullBooleanField()
+#    passed = models.BooleanField()
+
+    def test_standard_display(self):
+        for standard in TIME_STANDARDS.keys():
+            if str(standard) == str(self.test_standard):
+                return u'%s (%s ms)' % (standard, TIME_STANDARDS[standard])
+        return '?'
+
+
+def populate_meets_standard(*args, **kwargs):
+    instance = kwargs['instance']
+
+    if instance.runtime_ms and getattr(TIME_STANDARDS, instance.test_standard, False):
+        if instance.runtime_ms < TIME_STANDARDS[instance.test_standard]:
+            instance.meets_standard = True
+        else:
+            instance.meets_standard = False
+    else:
+        instance.meets_standard = None
+models.signals.pre_save.connect(populate_meets_standard, sender=TestRun)
